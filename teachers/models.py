5cfcbe6032b2b05365d87986ccd7c0358da9ca59
Chetan Puppala <manikanta.ions@gmail.com>
2020-01-26 19:35:42 +0530

    added teachers model

--- teachers/models.py
+++ teachers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from subjects.models import Subject
 
 # Create your models here.
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=255)
+    doj = models.DateField(auto_now_add=True)
+    subjects = models.ManyToManyField(Subject)
+    salary = models.IntegerField()
