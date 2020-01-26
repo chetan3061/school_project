d0a378dcb0c2b8ff60a9942f7dc0c8cb3ab37681
Chetan Puppala <manikanta.ions@gmail.com>
2020-01-26 19:17:25 +0530

    added ClassRoom model

--- classrooms/models.py
+++ classrooms/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ClassRoom(models.Model):
+    OVAL = 'OVL'
+    RECTANGLE = 'REC'
+    CANOPY = 'CAN'
+    ELEVATED = 'ELE'
+    shape_choices = [
+        (OVAL, 'Oval'),
+        (RECTANGLE, 'Rectangle'),
+        (CANOPY, 'Canopy'),
+        (ELEVATED, 'Elevated'),
+    ]
+    name = models.CharField(max_length=255)
+    capacity = models.IntegerField()
+    web_lecture_support = models.BooleanField(default=False)
+    shape = models.CharField(max_length=3, choices=shape_choices)
