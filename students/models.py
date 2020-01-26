ec2f2e611d1d62925a058fb88e156df284b75110
Chetan Puppala <manikanta.ions@gmail.com>
2020-01-27 02:13:58 +0530

    added Studen and PointOfContact models

--- students/models.py
+++ students/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    doj = models.DateField(auto_now_add=True)
+    standard = models.IntegerField()
+    roll_no = models.SlugField(unique=True)
+    ranking = models.IntegerField(null=True)
+
+
+class PointOfContact(models.Model):
+    name = models.CharField(max_length=255)
+    number = models.CharField(max_length=12)
+    relation = models.CharField(max_length=50)
+    student = models.ForeignKey(Student)
