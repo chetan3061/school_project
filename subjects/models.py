5405ad915e393acde532dea1098e0e019b229132
Chetan Puppala <manikanta.ions@gmail.com>
2020-01-26 18:42:28 +0530

    created subject model

--- subjects/models.py
+++ subjects/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=255)
+    chapters = models.IntegerField()
+    total_duration = models.IntegerField()
+    per_class_duration = models.IntegerField(
+        default=30,
+        validators=[MaxValueValidator(30), MinValueValidator(120)]
+    )
+    classroom = models.OneToOneField()
